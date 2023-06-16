1e50be6bef4f8cce48008ef43d6c549b923fb080
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2023-06-16 15:11:30 +0330

    fitness function defined, first generation created, member class created, code reformated

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import numpy as np
 from CnfResolver import CnfResolver
+import math
+import random
 
 cn = CnfResolver("input.cnf")
+# number of current generation
+mu = 2 * cn.nv
+# number of selected parent
+lmda = int(math.sqrt(mu))
+mutation_percent = 0.05
+top_membership_percent = 0.75
+bottom_membership_percent = 1 - top_membership_percent
 
-a = np.array([[1, 3, 4, 5],
-              [2, 4, 5, 6],
-              [1, 2, 3, 4],
-              [10, 20, 30, 40]])
-print(a[0:3][0:2])
+
+def fitness(cnf_valuation):
+    global cn
+
+    return cn.count_number_of_satisfactions(cnf_valuation)
+
+
+class Member:
+    def __init__(self, gene):
+        self.gene = gene
+        self.fitness = fitness(gene)
+
+
+def choose_parent(parent_list, generation_list):
+    parent_list.clear()
+    
+
+generation = [Member(np.random.choice([0, 1], size=cn.nv)) for i in range(mu)]
+generation.sort(key=lambda x: x.fitness)
+parent = []
+children = []
